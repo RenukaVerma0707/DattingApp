319d7ab22e32b0136c39e56728ed64aae0c5a45d
Renuka Verma <renuka.v3@tcs.com>
2021-10-26 09:35:56 +0530

    client

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,7 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnnectionString"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 
             app.UseAuthorization();
 
